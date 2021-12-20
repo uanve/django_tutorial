c952b11b0f100349fbc65f9e7e99904360db3296
uanve <46595907+uanve@users.noreply.github.com>
2021-12-20 11:15:01 +0100

    Update views.py

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    #return HttpResponse("Hello World")
-    return render(request,'hello.html')
+    return HttpResponse("Hello World!!!")
+    #return render(request,'hello.html')
